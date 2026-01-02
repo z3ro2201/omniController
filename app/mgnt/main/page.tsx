b8759ec32de149e88c534b7c8318060575dc7004
z3ro2201 <hello@2er0.io>
2026-01-02 15:44:49 +0900

    메인페이지 링크 추가

--- app/mgnt/main/page.tsx
+++ app/mgnt/main/page.tsx
@@ -1,5 +1,16 @@
+import Link from "next/link";
+
+import { AirVentIcon } from "lucide-react";
+
 const MainPage = () => {
-  return <></>;
+  return (
+    <>
+      <Link className="m-2 p-4 inline-block border border-gray-200 bg-white dark:border-white dark:text-white rounded-lg" href="/control/remote/airConditioner" target="_blank">
+        <AirVentIcon className="mr-2 inline-block" size={16} />
+        에어컨 리모컨
+      </Link>
+    </>
+  );
 };
 
 export default MainPage;
